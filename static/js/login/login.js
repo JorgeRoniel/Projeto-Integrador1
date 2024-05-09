76c4ecaa6d4a86dce4f155b84c56b45ebf0c8b55
Your Nelson Sales <nelsonsales20.ns@gmail.com>
2024-05-09 11:25:19 -0300

    telas de login e cadastro terminadas

--- static/js/login/login.js
+++ static/js/login/login.js
@@ -1,73 +1,42 @@
-const changeScreen = document.getElementById("criarConta");
-const formulario = document.getElementById("login-form");
-const campos = document.getElementById("campos");
-const options = document.querySelector(".cadastro");
+const mudarParaCadastro = document.getElementById("criarConta");
 const side = document.getElementById("side");
+const container = document.getElementById("formularioCampos");
+const cadastroScreen = document.getElementById("cadastro-form");
+const loginScreen = document.getElementById("campos");
+const naoTemConta = document.getElementById("naoTemConta");
+const mudarParaLogin = document.getElementById("logar");
+const jaTemConta = document.getElementById("jaTemConta");
+const mensagemVerificaSenha = document.getElementById("textLogin");
 
-changeScreen.addEventListener("click",function(){
-    campos.remove();
-    options.remove();
-    formulario.classList.add("cadastro-form");
-    side.classList.add("sideCadastro");
+cadastroScreen.remove();
+jaTemConta.remove();
 
-    const containerCampos = document.createElement("form");
+mudarParaCadastro.addEventListener("click", function(){
+    mensagemVerificaSenha.textContent="";
+    loginScreen.remove();
+    naoTemConta.remove();
+    container.classList.add("formMoveLeft");
+    container.appendChild(cadastroScreen);
+    container.appendChild(jaTemConta);
+    side.classList.add("sideMoveLeft");
+});
 
-    const apresentacao = document.createElement("div");
-    apresentacao.classList.add("apresentacao");
-    const imagem = document.createElement("img");
-    imagem.src = "static\\js\\login\\img\\codigo.png";
-    imagem.style.width = "15%";
-    const fraseCadastro = document.createElement("h1");
-    fraseCadastro.textContent = "Cadastre-se";
-    apresentacao.appendChild(imagem);
-    apresentacao.appendChild(fraseCadastro);
+mudarParaLogin.addEventListener("click",function(){
+    cadastroScreen.remove();
+    jaTemConta.remove();
+    container.classList.remove("formMoveLeft");
+    container.appendChild(loginScreen);
+    container.appendChild(naoTemConta);
+    side.classList.remove("sideMoveLeft");
+});
 
-    const quebraLinha1 = document.createElement("br");
-    const quebraLinha2 = document.createElement("br");
-    const quebraLinha3 = document.createElement("br");
-
-    const labUser = document.createElement("label");
-    labUser.textContent = "Nome Completo";
-    labUser.setAttribute("for","user");
-    const labEmail = document.createElement("label");
-    labEmail.textContent = "Email";
-    labEmail.setAttribute("for","email");
-    const labSenha = document.createElement("label");
-    labSenha.textContent = "Senha";
-    labSenha.setAttribute("for","senha");
-    const user = document.createElement("input");
-    user.id="user";
-    const email = document.createElement("input");
-    email.id="email";
-    const senha = document.createElement("input");
-    senha.id="senha";
-    const cadastrarButton = document.createElement("button");
-    cadastrarButton.textContent = "Cadastrar";
-    const validaSenha = document.createElement("p");
-    validaSenha.textContent = "";
-
-    const containerOption = document.createElement("div");
-    containerOption.classList.add("cadastro");
-    const fraseJaTemConta = document.createElement("p");
-    const logar = document.createElement("a");
-    fraseJaTemConta.textContent = "Já tem conta? |";
-    logar.textContent = " Entrar";
-    fraseJaTemConta.appendChild(logar);
-    containerOption.appendChild(fraseJaTemConta);
-
-    containerCampos.appendChild(apresentacao);
-    containerCampos.appendChild(labUser);
-    containerCampos.appendChild(user);
-    containerCampos.appendChild(quebraLinha1);
-    containerCampos.appendChild(labEmail);
-    containerCampos.appendChild(email);
-    containerCampos.appendChild(quebraLinha2);
-    containerCampos.appendChild(labSenha);
-    containerCampos.appendChild(senha);
-    containerCampos.appendChild(quebraLinha3);
-    containerCampos.appendChild(validaSenha);
-    containerCampos.appendChild(cadastrarButton);
-
-    formulario.appendChild(containerCampos);
-    formulario.appendChild(containerOption);
-})
+const check = () =>{
+    const user = document.getElementById("usuario").value;
+    const senha = document.getElementById("senha").value;
+    if(user === "nelson" && senha==="123"){
+        alert("login bem sucedido");
+    }
+    else{
+        mensagemVerificaSenha.textContent = "Usuário e/ou senha inválidos";
+    }
+}
