07286405799f06a48c220f6c2e611eed4d82e0c0
JorgeRoniel <jorge.infor4g@gmail.com>
2024-05-21 18:30:08 -0300

    Atualização: pegando dados via JSON diretamente pro backend

--- routes/rotas.py
+++ routes/rotas.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 
 rotas = Blueprint('rotas', __name__)
 
 @rotas.route('/')
 def login_page():
     return render_template('login.html')
+
+@rotas.route('/', methods=['POST'])
+def submit():
+
+    return redirect('/')
+
+@rotas.route('/', methods=['POST'])
+def create_user():
+
+    return redirect('/')
