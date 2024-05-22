1f21a68f9af40ffc6189c120eebc7b1be3f32b85
JorgeRoniel <jorge.infor4g@gmail.com>
2024-05-22 10:02:55 -0300

    Correções

--- routes/rotas.py
+++ routes/rotas.py
@@ -6,12 +6,25 @@ rotas = Blueprint('rotas', __name__)
 def login_page():
     return render_template('login.html')
 
-@rotas.route('/', methods=['POST'])
+@rotas.route('/submit', methods=['POST'])
 def submit():
-
+    if request.content_type == 'application/json':
+        data = request.get_json()
+    else:
+        data = request.form.to_dict()
+    
+    username = data.get('email')
+    password = data.get('senha')
+    print(username)
     return redirect('/')
 
-@rotas.route('/', methods=['POST'])
+@rotas.route('/create', methods=['POST'])
 def create_user():
-
+    if request.content_type == 'application/json':
+        data = request.get_json()
+    else:
+        data = request.form.to_dict()
+    
+    nome = data.get('novoNome')
+    print(nome)
     return redirect('/')
