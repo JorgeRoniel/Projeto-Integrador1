08b212616123995b6a3d0d25bc699265c1f2601d
Your Nelson Sales <nelsonsales20.ns@gmail.com>
2024-05-31 12:59:11 -0300

    design de filtro de itens implementado

--- static/js/homePage/home.js
+++ static/js/homePage/home.js
@@ -1,6 +1,13 @@
 const contaOpcoes = document.getElementById("contaOpcoes");
 const opcoes = document.getElementById('OpcoesExpandidas');
 const btn_logout = document.getElementById('logout');
+const maratonas = document.getElementById('maratonas');
+const competicoes = document.getElementById('competicoes');
+const times = document.getElementById('times');
+const underline = document.getElementById('underline');
+const containerBotaoCompeticoes = document.getElementById('containerBotaoCompeticoes');
+const containerBotaoMaratonas = document.getElementById('containerBotaoMaratonas');
+const containerBotaoTimes = document.getElementById('containerBotaoTimes');
 
 var checkExpansive = false; 
 
@@ -21,3 +28,18 @@ btn_logout.addEventListener('click', (event) => {
     fetch('/logout', {method: 'GET'})
         window.location.href = '/'
 })
+
+maratonas.addEventListener('click',function(){
+    containerBotaoMaratonas.appendChild(underline);
+})
+
+
+competicoes.addEventListener('click',function(){
+    containerBotaoCompeticoes.appendChild(underline);
+})
+
+times.addEventListener('click',function(){
+    containerBotaoTimes.appendChild(underline);
+})
+
+
