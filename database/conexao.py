d8ea65f05a05a19bcbeea6feb96ec4f8994c3d13
JorgeRoniel <jorge.infor4g@gmail.com>
2024-07-25 09:41:21 -0300

    informações sensiveis escondidas.

--- database/conexao.py
+++ database/conexao.py
@@ -1,12 +1,22 @@
 import pymysql
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv('DB_HOST')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
 
 def openBD():
     try:
-        connection = pymysql.connect(host='',
-                                user='',
-                                password='',
-                                database='',
-                                port=0,
+        connection = pymysql.connect(host=DB_HOST,
+                                user=DB_USER,
+                                password=DB_PASSWORD,
+                                database=DB_NAME,
+                                port=DB_PORT,
                                 cursorclass=pymysql.cursors.DictCursor)
         print('Conectado ao banco!')
         return connection
