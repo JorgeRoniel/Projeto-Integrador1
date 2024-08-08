169be08b98cd7add3ca1e8800f49e97e83fba9f8
Your Nelson Sales <nelsonsales20.ns@gmail.com>
2024-08-08 19:13:42 -0300

    Exibição de maratonas funcional

--- static/js/homePage/home.js
+++ static/js/homePage/home.js
@@ -8,7 +8,6 @@ const times = document.getElementById('times');
 const maratonasCreation = document.getElementById('maratonasCreation');
 const partidasCreation = document.getElementById('partidasCreation');
 const timesCreation = document.getElementById('timesCreation');
-const underline = document.getElementById('underline');
 const underlineCreation = document.getElementById("underlineCreation");
 const containerBotaopartidas = document.getElementById('containerBotaopartidas');
 const containerBotaoMaratonas = document.getElementById('containerBotaoMaratonas');
@@ -22,79 +21,138 @@ const criarMaratona = document.getElementById("criarMaratona");
 const criarTime = document.getElementById("criarTime");
 const criarPartida = document.getElementById("criarPartida");
 const formMaratona = document.getElementById("criar-maratona-form");
+const exibirCategorias = document.getElementById("ExibirCategorias");
+const filter = document.getElementById("filter");
 
-var checkExpansive = false; 
+const underline = document.createElement('hr');
+underline.classList.add("horizontal-bar");
+underline.style.marginTop = "0";
+underline.style.marginBottom = "0";
+underline.style.width = "100%";
+underline.style.height = "2px";
 
-contaOpcoes.addEventListener('click',function(){
-    if(checkExpansive === false){
-        opcoes.style.display="flex";
-        checkExpansive=true;
+//Variaveis que dirão qual está sendo exibido na tela na hora de filtrar
+var getMaratona = false;
+var getTimes = false;
+var getPartidas = false;
+
+//Checa se a caixa de logout está expandida ou não
+var checkExpansive = false;
+
+//Vetores que guardarão in memory os gets para tornar o programa performático
+var maratonasSalvas = [];
+var timesSalvos = [];
+var partidasSalvas = [];
+
+//Variáveis que contarão a quantidade de inserts para checar se faz a consulta ou não
+var countInsertMaratona = 0;
+var countInsertTimes = 0;
+var countInsertSalvos = 0;
+
+contaOpcoes.addEventListener('click', function () {
+    if (checkExpansive === false) {
+        opcoes.style.display = "flex";
+        checkExpansive = true;
     }
-    else{
-        opcoes.style.display="none";
+    else {
+        opcoes.style.display = "none";
         checkExpansive = false;
     }
 })
 
-fecharCriacao.addEventListener('click',function(){
+fecharCriacao.addEventListener('click', function () {
     criacao.style.display = "none";
 })
 
-criarMaisButton.addEventListener('click',function(){
+criarMaisButton.addEventListener('click', function () {
     criacao.style.display = "flex";
 })
 
 btn_logout.addEventListener('click', (event) => {
     event.preventDefault();
 
-    fetch('/logout', {method: 'GET'})
-        window.location.href = '/'
+    fetch('/logout', { method: 'GET' })
+    window.location.href = '/'
 })
 
-maratonas.addEventListener('click', function(event){
+maratonas.addEventListener('click', async function (event) {
+    getMaratona = true;
+    getTimes = false;
+    getPartidas = false;
+    filter.value = '';
+
+    exibirCategorias.innerHTML = '';
     containerBotaoMaratonas.appendChild(underline);
 
-    const options = {
-        method: 'GET'
-    }
+    //Se a quantidade de inserts for maior que a quantidade de maratonas salvas, então ele deve realizar a consulta, pq há insert novo
+    //Se o vetor de maratoans salvas tiver tamanho 0, significa que nenhuma consulta foi feita ainda, então precisa consultar.
+    if (countInsertMaratona > maratonasSalvas.length || maratonasSalvas.length === 0) {
+        const options = {
+            method: 'GET'
+        }
+        const maratonasSalvasBackup = maratonasSalvas; //Cria um vetor auxiliar para backup caso a consulta falhe
+        maratonasSalvas = []; //Esvazia o vetor original para evitar duplicidade de itens
+        await fetch('/maratonas', options)
+            .then(response => response.json())
+            .then(data => {
+                console.log('Maratonas:', data);
 
-    fetch('/maratonas', options)
-        .then(response => response.json())
-        .then(data => {
-            console.log('Maratonas:', data); 
+                data.forEach(maratona => {
+                    const maratonasObj = {
+                        nome: maratona.nome_maratona,
+                        descricao: maratona.descricao,
+                        qtdTimes: maratona.qtdTimes,
+                        premiacao: maratona.premiacao,
+                    }
 
-            const maratonasContainer = document.getElementById('listarMaratonas');
-            maratonasContainer.innerHTML = ''; 
-
-            data.forEach(maratona => {
-                const maratonaDiv = document.createElement('div');
-                maratonaDiv.classList.add('maratona');
-
-                maratonaDiv.innerHTML = `
-                    <h3>${maratona.nome_maratona}</h3>
-                    <p>Descrição: ${maratona.descricao}</p>
-                    <p>Quantidade de Times: ${maratona.qtdTimes}</p>
-                    <p>Premiação: ${maratona.premiacao}</p>
-                `;
-
-                maratonasContainer.appendChild(maratonaDiv);
+                    maratonasSalvas.push(maratonasObj);
+                });
+            })
+            .catch((error) => {
+                maratonasSalvas = maratonasSalvasBackup;
+                console.error('ERRO: ', error);
             });
-        })
-        .catch((error) => {
-            console.error('ERRO: ', error);
-        });
+        //Se o countInsertMaratona for = 0, na maioria dos casos, significará que é a primeira consulta,
+        //então é atribuído a ele o tamanho do vetor para a comparação funcionar corretamente.
+        countInsertMaratona = countInsertMaratona === 0 ? maratonasSalvas.length : countInsertMaratona;
+    }
+    maratonasSalvas.forEach(element => {
+        const containerItem = document.createElement('li');
+        const linhaDivisoria = document.createElement('hr');
+        linhaDivisoria.classList.add("horizontal-bar");
+        linhaDivisoria.style.marginTop = "0";
+        linhaDivisoria.style.marginBottom = "0";
+        linhaDivisoria.style.width = "100%";
+        linhaDivisoria.style.height = "2px";
+        containerItem.innerHTML = `
+            // <h3>${element.nome}</h3>
+            // <p>Descrição: ${element.descricao}</p>
+            // <p>Quantidade de Times: ${element.qtdTimes}</p>
+            // <p>Premiação: ${element.premiacao}</p>
+            // `;
+        exibirCategorias.appendChild(containerItem);
+        exibirCategorias.appendChild(linhaDivisoria);
+    });
 });
 
 
-partidas.addEventListener('click',function(){
+partidas.addEventListener('click', function () {
+    getMaratona = false;
+    getTimes = false;
+    getPartidas = true;
     containerBotaopartidas.appendChild(underline);
+    exibirCategorias.innerHTML = '';
 })
 
-times.addEventListener('click',function(){
+times.addEventListener('click', function () {
+    getMaratona = false;
+    getTimes = true;
+    getPartidas = false;
     containerBotaoTimes.appendChild(underline);
+    exibirCategorias.innerHTML = '';
 })
 
-maratonasCreation.addEventListener('click',function(){
+maratonasCreation.addEventListener('click', function () {
     containerBotaoMaratonasCreation.appendChild(underlineCreation);
     criarMaratona.style.display = "flex";
     criarPartida.style.display = "none";
@@ -102,21 +160,21 @@ maratonasCreation.addEventListener('click',function(){
 })
 
 
-partidasCreation.addEventListener('click',function(){
+partidasCreation.addEventListener('click', function () {
     containerBotaopartidasCreation.appendChild(underlineCreation);
     criarMaratona.style.display = "none";
     criarPartida.style.display = "flex";
     criarTime.style.display = "none";
 })
 
-timesCreation.addEventListener('click',function(){
+timesCreation.addEventListener('click', function () {
     containerBotaoTimesCreation.appendChild(underlineCreation);
     criarMaratona.style.display = "none";
     criarPartida.style.display = "none";
     criarTime.style.display = "flex";
 })
 
-formMaratona.addEventListener('submit', (event) => {
+formMaratona.addEventListener('submit', async function (event) {
     event.preventDefault();
 
     const formData = new FormData(formMaratona);
@@ -134,15 +192,16 @@ formMaratona.addEventListener('submit', (event) => {
         body: JSON.stringify(data),
     }
 
-    fetch('/createMarathon', options)
+    await fetch('/createMarathon', options)
         .then(response => response.json())
         .then(data => {
-            if(data.status === "success"){
+            if (data.status === "success") {
+                countInsertMaratona += 1;
                 alert(data.message);
-            }else{
-                if(data.status === "error"){
+            } else {
+                if (data.status === "error") {
                     alert("erro ao criar maratona");
-                }else if(data.status == "N/A"){
+                } else if (data.status == "N/A") {
                     alert(data.message);
                 }
             }
@@ -151,4 +210,35 @@ formMaratona.addEventListener('submit', (event) => {
             console.log("Erro: ", error);
         });
 });
+
+filter.addEventListener("input", function (event) {
+    exibirCategorias.innerHTML = '';
+    if (getMaratona) {
+        maratonasSalvas.forEach(element => {
+            if (element.nome.toLowerCase().includes(event.target.value.toLowerCase())) {
+                const containerItem = document.createElement('li');
+                const linhaDivisoria = document.createElement('hr');
+                linhaDivisoria.classList.add("horizontal-bar");
+                linhaDivisoria.style.marginTop = "0";
+                linhaDivisoria.style.marginBottom = "0";
+                linhaDivisoria.style.width = "100%";
+                linhaDivisoria.style.height = "2px";
+                containerItem.innerHTML = `
+                    // <h3>${element.nome}</h3>
+                    // <p>Descrição: ${element.descricao}</p>
+                    // <p>Quantidade de Times: ${element.qtdTimes}</p>
+                    // <p>Premiação: ${element.premiacao}</p>
+                    // `;
+                exibirCategorias.appendChild(containerItem);
+                exibirCategorias.appendChild(linhaDivisoria);
+            }
+        });
+    }
+    else if (getPartidas) {
+
+    }
+    else if (getTimes) {
+
+    }
+})
 
