3e7654c9317714f41eb266862a8ca5e89534218a
JorgeRoniel <jorge.infor4g@gmail.com>
2024-09-15 15:31:21 -0300

    Serviços de Partidas.

--- database/Models.py
+++ database/Models.py
@@ -17,15 +17,13 @@ class Marathon:
 
 #classe da partida
 class Match:
-    def __init__(self, date, hour, local, blueTeam, redTeam, score, timeOfScore, stats):
+    def __init__(self, date, local, blueTeam, redTeam, winner):
         self.date = date
-        self.hour = hour
         self.local = local
         self.blueTeam = blueTeam
         self.redTeam = redTeam
-        self.score = score
-        self.timeOfScore = timeOfScore
-        self.stats = stats
+        self.winner = winner
+        
 
 #classe dos times
 class Team:
@@ -40,56 +38,6 @@ class Player:
         self.name = name
         self.teamId = teamId
         
-#definição de arvore e da classe para arvore de chaveamento
-class NoArvore:
-    def __init__(self, Match=None):
-        self.Match = Match
-        self.left = None
-        self.right = None
-
-class ChaveamentoArvore:
-    def __init__(self):
-        self.root = None
-
-    def insert_match(self, match):
-        if not self.root:
-            self.root = NoArvore(match)
-        else:
-            self._insert_match(match, self.root)
-
-    def _insert_match(self, match, no_atual):
-        if not no_atual.left:
-            no_atual.left = NoArvore(match)
-        elif not no_atual.right:
-            no_atual.right = NoArvore(match)
-        else:
-            # Se ambos os lados estão ocupados, descemos recursivamente em um dos lados
-            self._insert_match(match, no_atual.left)
-
-    def print_chaveamento(self):
-        self._print_chaveamento(self.root)
-
-    def _print_chaveamento(self, no_atual):
-        if no_atual:
-            print("Partida:", no_atual.Match.blueTeam, "vs", no_atual.Match.redTeam)
-            self._print_chaveamento(no_atual.left)
-            self._print_chaveamento(no_atual.right)
-
-def retirar_trecho(original, inicio, fim):
-    """
-    Remove o trecho da string original entre as posições de início e fim (não inclusivo).
-    
-    Parâmetros:
-    original (str): A string original.
-    inicio (int): A posição inicial do trecho a ser removido.
-    fim (int): A posição final do trecho a ser removido (não inclusivo).
-    
-    Retorna:
-    str: A string resultante após a remoção do trecho.
-    """
-    trecho_removido = original[inicio:fim]
-
-    return trecho_removido
 
 class Team_le:
     def __init__(self, TId, name, shield, nick, teamMates):
--- services/CompeticaoService.py
+++ services/CompeticaoService.py
@@ -1,13 +1,68 @@
 from database import conexao as c
+from Estruturas import ArvoreBinaria as ar
+from database import Models as m
 
-def criarCompeticao():
-    pass
+def criarCompeticao(data_partida, local, id_time1, id_time2, id_vencedor, maratonaId):
+    try:
+        conn = c.openBD()
+        cursor = conn.cursor()
 
-def atualizarCompetição():
-    pass
+        cursor.execute(f"INSERT INTO partidas (data_partida, local_partida, time1, time2, vencedor, maratonaId) VALUES ('{data_partida}', '{local}', {id_time1}, {id_time2}, {id_vencedor}, {maratonaId});")
+        conn.commit()
+        conn.close()
 
-def listarCompeticoes():
-    pass
+        return True
+    except Exception as e:
+        print(e)
+        return False
 
-def deletarCompeticao():
-    pass
+def atualizarCompetição(id, nova_data, novo_local, novo_time1, novo_time, novo_vencedor):
+    try:
+        conn = c.openBD()
+        cursor = conn.cursor()
+
+        sql = "UPDATE partidas SET data_partida=%s, local_partida=%s, time1=%s, time2=%s, vencedor=%s WHERE id = %s;"
+
+        cursor.execute(sql, (nova_data, novo_local, novo_time1, novo_time, novo_vencedor, id))
+        conn.commit()
+        conn.close()
+
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def listarCompeticoes(id_maratona):
+    try:
+        conn = c.openBD()
+        cursor = conn.cursor()
+
+        cursor.execute(f"SELECT p.data_partida, p.local_partida, p.time1, p.time2 FROM partidas p INNER JOIN maratona m ON m.id = {id_maratona};")
+        data = cursor.fetchall()
+
+        arvore = ar.ArvoreBinaria()
+
+        for dado in data:
+            partida = m.Match(dado['data_partida'], dado['local_partida'], dado['time1'], dado['time2'], dado['vencedor'])
+            arvore.inserir(partida)
+
+        return arvore
+    except Exception as e:
+        print(e)
+        return []
+
+def deletarCompeticao(id_partida):
+    try:
+        conn = c.openBD()
+        cursor = conn.cursor()
+
+        sql = "DELETE FROM competidores WHERE id = %s;"
+
+        cursor.execute(sql, (id_partida))
+        conn.commit()
+        conn.close()
+
+        return True
+    except Exception as e:
+        print(e)
+        return False
